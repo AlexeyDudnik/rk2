8938c07adc2b1d55269b12b54cfbc81bfef48809
AlexDudnik <145279680+AlexeyDudnik@users.noreply.github.com>
2024-05-20 23:07:41 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,158 +1,133 @@
 #include <iostream>
 #include <sstream>
 
-class Handler
-{
+class Handler {
 public:
-  virtual ~Handler() {}
-  
-  virtual void setHandler( Handler *s )
-  {
-    successor = s;
-  }
-  
-virtual void handleRequest()
-  {
-    if (successor != nullptr)
-    {
-      successor->handleRequest();
-    }else{
-        // Обработка случая, когда указатель не инициализирован или нулевой
+    virtual ~Handler() {}
+
+    virtual void setHandler(Handler* s) {
+        successor = s;
     }
-  }
-  // ...
+
+    virtual void handleRequest() {
+        if (successor != nullptr) {
+            successor->handleRequest();
+        }
+    }
 
 private:
-  Handler *successor;
+    Handler* successor = nullptr;
 };
 
 /*
  * Concrete Handlers
  * handle requests they are responsible for
  */
-class ConcreteHandler1 : public Handler
-{
+class ConcreteHandler1 : public Handler {
 public:
-  ~ConcreteHandler1() {}
-  
-  bool canHandle()
-  {
-    // ...
-    return false;
-  }
-  
-  virtual void handleRequest()
-  {
-    if ( canHandle() )
-    {
-      std::cout << "Handled by Concrete Handler 1" << std::endl;
+    ~ConcreteHandler1() {}
+
+    bool canHandle() {
+        // Simulate some logic to determine if this handler can handle the request
+        return false;
     }
-    else
-    {
-      std::cout << "Cannot be handled by Handler 1" << std::endl;
-      Handler::handleRequest();
+
+    virtual void handleRequest() override {
+        if (canHandle()) {
+            std::cout << "Handled by Concrete Handler 1" << std::endl;
+        } else {
+            std::cout << "Cannot be handled by Handler 1" << std::endl;
+            Handler::handleRequest();
+        }
     }
-    // ...
-  }
-  // ...
 };
 
-class ConcreteHandler2 : public Handler
-{
+class ConcreteHandler2 : public Handler {
 public:
-  ~ConcreteHandler2() {}
-  
-  bool canHandle()
-  {
-    // ...
-    return canHandleValue;
-  }
-  
-  void setCanHandleValue(bool value)
-  {
-    canHandleValue = value;
-  }
-  
-  virtual void handleRequest()
-  {
-    if ( canHandle() )
-    {
-      std::cout << "Handled by Handler 2" << std::endl;
+    ~ConcreteHandler2() {}
+
+    bool canHandle() {
+        return canHandleValue;
     }
-    else
-    {
-      std::cout << "Cannot be handled by Handler 2" << std::endl;
-      Handler::handleRequest();
+
+    void setCanHandleValue(bool value) {
+        canHandleValue = value;
     }
-    // ...
-  }
-  
+
+    virtual void handleRequest() override {
+        if (canHandle()) {
+            std::cout << "Handled by Handler 2" << std::endl;
+        } else {
+            std::cout << "Cannot be handled by Handler 2" << std::endl;
+            Handler::handleRequest();
+        }
+    }
+
 private:
-  bool canHandleValue = true; // изначальное значение
+    bool canHandleValue = true;
 };
-
 
 void test1() {
-  std::stringstream buffer;
-  std::streambuf *sbuf = std::cout.rdbuf();
-  std::cout.rdbuf(buffer.rdbuf());
+    std::stringstream buffer;
+    std::streambuf* sbuf = std::cout.rdbuf();
+    std::cout.rdbuf(buffer.rdbuf());
 
-  ConcreteHandler1 handler1;
-  ConcreteHandler2 handler2;
-  
-  handler1.setHandler( &handler2 );
-  handler1.handleRequest();
+    ConcreteHandler1 handler1;
+    ConcreteHandler2 handler2;
 
-  std::string output = buffer.str();
-  std::cout.rdbuf(sbuf);
+    handler1.setHandler(&handler2);
+    handler1.handleRequest();
 
-  if (output == "Cannot be handled by Handler 1nHandled by Handler 2n") {
-    std::cout << "Test 1 passedn";
-  } else {
-    std::cout << "Test 1 failedn";
-  }
+    std::string output = buffer.str();
+    std::cout.rdbuf(sbuf);
+
+    if (output == "Cannot be handled by Handler 1\nHandled by Handler 2\n") {
+        std::cout << "Test 1 passed\n";
+    } else {
+        std::cout << "Test 1 failed\n";
+    }
 }
+
 void test2() {
-  std::stringstream buffer;
-  std::streambuf *sbuf = std::cout.rdbuf();
-  std::cout.rdbuf(buffer.rdbuf());
+    std::stringstream buffer;
+    std::streambuf* sbuf = std::cout.rdbuf();
+    std::cout.rdbuf(buffer.rdbuf());
 
-  ConcreteHandler1 handler1;
-  handler1.handleRequest();
+    ConcreteHandler1 handler1;
+    handler1.handleRequest();
 
-  std::string output = buffer.str();
-  std::cout.rdbuf(sbuf);
+    std::string output = buffer.str();
+    std::cout.rdbuf(sbuf);
 
-  if (output == "Cannot be handled by Handler 1n") {
-    std::cout << "Test 2 passedn";
-  } else {
-    std::cout << "Test 2 failedn";
-  }
+    if (output == "Cannot be handled by Handler 1\n") {
+        std::cout << "Test 2 passed\n";
+    } else {
+        std::cout << "Test 2 failed\n";
+    }
 }
 
 void test3() {
-  std::stringstream buffer;
-  std::streambuf *sbuf = std::cout.rdbuf();
-  std::cout.rdbuf(buffer.rdbuf());
+    std::stringstream buffer;
+    std::streambuf* sbuf = std::cout.rdbuf();
+    std::cout.rdbuf(buffer.rdbuf());
 
-  ConcreteHandler2 handler2;
-  handler2.handleRequest();
+    ConcreteHandler2 handler2;
+    handler2.handleRequest();
 
-  std::string output = buffer.str();
-  std::cout.rdbuf(sbuf);
+    std::string output = buffer.str();
+    std::cout.rdbuf(sbuf);
 
-  if (output == "Handled by Handler 2n") {
-    std::cout << "Test 3 passedn";
-  } else {
-    std::cout << "Test 3 failedn";
-  }
+    if (output == "Handled by Handler 2\n") {
+        std::cout << "Test 3 passed\n";
+    } else {
+        std::cout << "Test 3 failed\n";
+    }
 }
 
-int main()
-{
+int main() {
     test1();
     test2();
     test3();
-    
+
     return 0;
 }
