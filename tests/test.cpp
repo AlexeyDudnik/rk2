8a47388afd2bd1043f83054e661222abda82aea4
AlexDudnik <145279680+AlexeyDudnik@users.noreply.github.com>
2024-05-20 22:55:26 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -110,28 +110,40 @@ void test1() {
     std::cout << "Test 1 failedn";
   }
 }
+void test2() {
+  std::stringstream buffer;
+  std::streambuf *sbuf = std::cout.rdbuf();
+  std::cout.rdbuf(buffer.rdbuf());
 
-void test2()
-{
-    ConcreteHandler1 handler1;
-    ConcreteHandler2 handler2;
-    
-    handler1.setHandler(&handler2);
-    handler2.handleRequest();
-    std::cout << "Тест 2 пройден: ConcreteHandler2 обработал запрос" << std::endl;
+  ConcreteHandler1 handler1;
+  handler1.handleRequest();
+
+  std::string output = buffer.str();
+  std::cout.rdbuf(sbuf);
+
+  if (output == "Cannot be handled by Handler 1n") {
+    std::cout << "Test 2 passedn";
+  } else {
+    std::cout << "Test 2 failedn";
+  }
 }
 
-void test3()
-{
-    ConcreteHandler1 handler1;
-    ConcreteHandler2 handler2;
-    
-    // Устанавливаем, что handler2 не может обработать запрос
-    handler2.setCanHandleValue(false);
-    
-    handler1.setHandler(&handler2);
-    handler1.handleRequest();
-    std::cout << "Тест 3 пройден: Ни один из обработчиков не может обработать запрос" << std::endl;
+void test3() {
+  std::stringstream buffer;
+  std::streambuf *sbuf = std::cout.rdbuf();
+  std::cout.rdbuf(buffer.rdbuf());
+
+  ConcreteHandler2 handler2;
+  handler2.handleRequest();
+
+  std::string output = buffer.str();
+  std::cout.rdbuf(sbuf);
+
+  if (output == "Handled by Handler 2n") {
+    std::cout << "Test 3 passedn";
+  } else {
+    std::cout << "Test 3 failedn";
+  }
 }
 
 int main()
